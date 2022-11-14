06b70cbf546426a6f94f47eda31bae6845acf49f
Devnovato76 <devnovato76@gmail.com>
2022-11-14 17:38:43 +0100

    Agregado Ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+const nombre = "Fernando";
+let edad = 46;
+let desarrollador = false;
+
+const miFechaNacimiento = new Date(1976, 0, 22);
+
+const miLibroFavorito = {
+  titulo: "La Casa de los espiritus",
+  autor: "Isabel Allende",
+  fecha: new Date(1982, 5, 15),
+  url: "htttps://miLibroFavorito.com",
+};
